f146d82c7acfd85de6a48a48deb3874db8cae585
itzbinz6 <abisolaajayi360@gmail.com>
2025-01-21 22:27:06 +0100

    Add files via upload

--- ass.java
+++ ass.java
@@ -2,21 +2,81 @@ import java.util.Scanner;
 
 public class ass{
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
+        /* Ajayi Abisola Enoch */
+        /* 2302025, 23CD010103 */
+        Scanner input = new Scanner(System.in);
         double mean, total_num, part_1, part_2 ,part_3;
         Double[] numbers={2.0,5.0,5.0,9.0,4.0,7.0,0.0,9.0,6.0,11.0,12.0};
-        /*No. 1 */
-        /*For The Standard deviation */
+
+
+         /*No. 1*/
+        /*For the Nigerian flag */
+        for(int j=0;j<4;j++){
+            System.out.println("****====****");
+        }/*End of the single loop*/
+        System.out.println("End of single loop");
+        System.out.println();
+        int width=12;
+        int height=4;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if(j<width/3){
+                    System.out.print("*");
+                }else if (j<2*width/3) {
+                    System.out.print("=");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }/*End of nested loop*/
+        System.out.println();
+
+        /*N0. 2 */
+        int rows=13;
+        int starsperrow=6;
+        int totalstars=30;
+        int starsinlastrow=totalstars-(starsperrow*(rows-1));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < (i<rows-1? starsperrow:starsinlastrow); j++) {
+                System.out.print("*");   
+            }
+            for (int j = 0; j < rows; j++) {
+                System.out.print("=");
+            }
+            System.out.println();
+        }/* End of single loop */
+        System.out.println("End of single loop");
+        System.out.println();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < starsperrow; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < 30; j++) {
+                if(i%2==0){
+                    System.out.print("=");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        /*No. 3 */
+        /*For The Standard deviation */ 
         total_num=0;
         part_1=0;
         for(Double number:numbers){
             total_num=total_num+number;
         }
-        mean=total_num/numbers.length;
+        mean=total_num/numbers.length; 
         for(Double number:numbers){
             part_1+=Math.pow(number-mean,2);
         }
-        
         /*Median */
         double n=numbers.length;
         /*For sorting */
@@ -41,14 +101,10 @@ public class ass{
         part_3=Math.sqrt(part_2);
         System.out.printf("The sd is %.2f",part_3);
         System.out.print("\n");
-        System.out.println("The median is"+median);        
-        /*No. 2 */
-        /*For the flag */
-        for(int j=0;j<4;j++){
-            System.out.println("****====****");
-        }
-         /*Question No.3 */
-        
+        System.out.println("The median is"+median); 
+        System.out.println();
+
+        /*No. 4*/
         int[]num=new int[10];
         System.out.println("You can only input "+num.length+" numbers");
         for (int i = 0; i < 10; i++) {
@@ -61,7 +117,9 @@ public class ass{
         for (int i = 0; i <=9; i=i+1) {
             System.out.print(num[i]+", ");
         }
-        /*Question No. 4 */
+        System.out.println();
+
+        /*Question No. 5 */
         System.out.print("\n");
         Scanner scanner =new Scanner(System.in);
         int[][]array_1 = new int[10][10];
@@ -69,7 +127,6 @@ public class ass{
             for(int j=0; j<10; j++){
                 System.out.println("Enter your value for row = "+ (i+1) +" col = " + (j+1));
                 array_1[i][j]=scanner.nextInt();
-                
             }
         }
         System.out.println("The elements you entered are:");
@@ -81,3 +138,4 @@ public class ass{
                 }
     }
 }
+/* Ajayi Abisola Enoch */
